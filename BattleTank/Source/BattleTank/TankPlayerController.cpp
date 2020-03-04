49e885c142497f771339a056d92c2056799579c3
PSI Metalify <psimetalify@gmail.com>
2020-03-04 23:15:51 +0100

    Everything is not broken but reticle logger is not quite there yet.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -17,7 +17,7 @@ void ATankPlayerController::Tick(float DeltaTime)
 {
 //super
 	Super::Tick(DeltaTime);
-	{	UE_LOG ( LogTemp, Warning, TEXT ("tankcontroller is ticking") ) }
+	//{	UE_LOG ( LogTemp, Warning, TEXT ("tankcontroller is ticking") ) }
 	//aim towards reticle
 	AimTowardsCrosshair();
 }
@@ -31,7 +31,44 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	//get world location if linetrace through crosshair
-	// if it hits the landscape
-		// tell controlled tank to aim at this point
+	FVector OutHitLocation; //Out parameter
+	if (GetSightRayHitLocation(OutHitLocation))// has "side-effect", will raytrace
+	{
+		// UE_LOG(LogTemp, Warning, TEXT("HitLocation: %S"), *OutHitLocation.ToString());
+		// TODO Tell controlled tank to aim at this point
+	}
 }
+
+// Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	// "De-project" the screen position of the crosshair to a world direction
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	FVector out_WorldOrigin;
+	FVector out_WorldDirection;
+
+	void 	DeprojectScreenToWorld
+	(
+		const FVector2D & ScreenPos,
+		const FIntRect & ViewRect,
+		const FMatrix & InvViewMatrix,
+		const FMatrix & InvProjMatrix,
+		FVector & out_WorldOrigin,
+		FVector & out_WorldDirection
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT ("%s"), *out_WorldDirection.ToString())
+
+
+	//UE_LOG(LogTemp, Warning, TEXT("Screenlocation = %s"), *ScreenLocation.ToString());
+	
+	// Line-trace along that look direction
+	return true;
+}
+
